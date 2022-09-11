9c1c1ef0380886202d9aa10a9275ed59f8821413
dev-rahim <rahimuddin5915@gmail.com>
2022-09-11 16:21:14 +0600

    exploring json fetch data

--- api.js
+++ api.js
@@ -1,5 +1,21 @@
 function LoadData() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
+
+function LoadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => DisplayUsers(data));
+}
+
+function LoadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+function DisplayUsers(data) {
+    console.log(data);
 }
